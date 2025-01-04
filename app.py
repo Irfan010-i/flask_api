b14caad8b830409d17e90a2c7034e84072d4fdad
Irfan Shaikh <haikh619@gmail.com>
2025-01-04 13:20:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -18,5 +18,6 @@ def predict():
     prediction = model.predict([input_data])
     return jsonify({'prediction': int(prediction[0])})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
